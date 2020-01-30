813b9a8a81ffffc3ed0afb41ef729183881d60da
samyuktha <samyukthasam511@gmail.com>
2020-01-30 05:42:43 +0530

    Front end corrected

--- app_one/views.py
+++ app_one/views.py
@@ -2,12 +2,16 @@ import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 
+BASE_CRAIGSLIST_URL = 'https://chennai.craigslist.org/search/ccc?query={}'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    response = requests.get('https://chennai.craigslist.org/search/ccc?query=MUSIC&sort=rel')
+    data = response.text
+    print(data)
     stuff_for_frontend = {
           'search': search,
     }
